679b4c6decd916c594e84eb0c55e74f9abba4279
Dmitry Demin <dmitry.demin@megafon.ru>
2022-02-15 09:37:40 +0500

    Lesson 2. CSV, Json, Yaml

--- lesson_2_1.py
+++ lesson_2_1.py
@@ -1,8 +1,11 @@
 import csv
+import datetime
 import os.path
 import re
+import json
 
 import chardet
+import yaml
 
 
 def detect_encoding(filename: str) -> str:
@@ -60,9 +63,66 @@ def write_to_csv(filenames_list: list, csv_filename: str):
             writer.writerow(row)
 
 
+def write_order_to_json(item: str, quantity: int, price: float, buyer: str, date: datetime.datetime):
+    json_file = 'tmp/orders.json'
+    params = [item, quantity, price, buyer, date]
+
+    with open(json_file, 'r', encoding='utf-8') as f:
+        orders_obj = json.loads(f.read())
+
+    order = {}
+    if all(params):
+        order['item'] = item
+        order['quantity'] = quantity
+        order['price'] = price
+        order['buyer'] = buyer
+        order['date'] = date.strftime('%d.%m.%Y')
+
+    orders_obj['orders'].append(order)
+    with open(json_file, 'w', encoding='utf-8') as f:
+        json.dump(orders_obj, f, indent=4)
+
+
+def save_obj_to_yaml(obj_dict: dict, filepath: str):
+    with open(filepath, 'w', encoding='utf-8') as f:
+        data = yaml.dump(obj_dict, f, sort_keys=False, default_flow_style=False, allow_unicode=True)
+    return data
+
+
+def load_obj_from_yaml(filepath: str) -> dict:
+    with open(filepath, 'r', encoding='utf-8') as f:
+        data = yaml.load(f, Loader=yaml.SafeLoader)
+    return data
+
+
 if __name__ == '__main__':
     filenames_list = ['info_1.txt', 'info_2.txt', 'info_3.txt']
     tmp_dir = 'tmp'
     files = [os.path.join(tmp_dir, f) for f in filenames_list]
     csvfilename = 'output.csv'
     write_to_csv(files, csvfilename)
+
+    orders = [
+        ['Milk', 4, 43.7, 'Dmitry D.', datetime.datetime.now()],
+        ['Bread', 1, 28.5, 'Dmitry D.', datetime.datetime.now()],
+    ]
+    for order in orders:
+        write_order_to_json(*order)
+
+    yaml_obj = {
+        'list': [1, 2, 3],
+        4: 4,
+        'euros': {
+            '1€': '1 €',
+            '2€': '2 €',
+            '3€': '3 €',
+        }
+    }
+
+    yaml_file = 'tmp/file.yaml'
+    save_obj_to_yaml(yaml_obj, yaml_file)
+
+    res_yaml = load_obj_from_yaml(yaml_file)
+    print(yaml_obj)
+    print(res_yaml)
+    assert res_yaml == yaml_obj
