03fcea35915940fd8ca808975287623e3e30baf1
Dmitry Demin <dmitry.demin@megafon.ru>
2022-03-24 16:04:16 +0500

    Lesson 8. Peer to peer messaging

--- lesson_8/server.py
+++ lesson_8/server.py
@@ -13,46 +13,65 @@ logger = logging.getLogger('server')
 
 
 @log
-def process_client_message(message: dict) -> dict:
+def process_client_message(message: dict, client, names: dict) -> dict:
+    answer_bad_request = {
+        "response": RESP_WRONG_REQUEST,
+        "error": "Bad Request"
+    }
     if "action" in message and message['action'] == 'presence'\
             and 'time' in message and 'user' in message:
-        answer = {
-            "response": RESP_OK,
-        }
-        return answer
+        message["response"] = RESP_OK
+        names[message['user']['account_name']] = client
+        send_message(client, message)
+        return message
+    elif "action" in message and message['action'] == 'msg'\
+            and 'time' in message and 'user' in message and 'to' in message:
+        if message['to'] not in names.keys():
+            answer = {
+                "response": RESP_NOT_FOUND,
+                "error": "This user is not online!"
+            }
+            send_message(client, answer)
+            return answer
+        message["response"] = RESP_OK
+        return message
     else:
-        answer = {
-            "response": RESP_WRONG_REQUEST,
-            "error": "Bad Request"
-        }
-        return answer
+        logger.warning(f'Bad Request from {client.getpeername()}')
+        return answer_bad_request
 
 
-def read_requests(r_clients, all_clients):
+def read_requests(r_clients, all_clients, names: dict):
     """Read requests from list of clients"""
     responses = {}
     for sock in r_clients:
         client_n = sock.fileno()
         client_peername = sock.getpeername()
         try:
-            responses[sock] = get_message(sock)
+            new_message = get_message(sock)
+            print('NEW', new_message)
+            responses[sock] = process_client_message(new_message, sock, names)
         except:
             logger.info(f'Client {client_n} {client_peername} was disconnected.')
             all_clients.remove(sock)
     return responses
 
 
-def write_responses(requests, w_clients, all_clients):
+def write_responses(requests, w_clients, all_clients, names: dict):
     for sock in w_clients:
         if sock in requests:
             try:
                 client_message = requests[sock]
-                if 'msg' in client_message:
-                    client_message['response'] = 200
-                    send_message(sock, client_message)
+                if 'msg' in client_message and 'to' in client_message:
+                    to_client = names[client_message['to']]
+                    if client_message['to'] in names.keys():
+                        send_message(to_client, client_message)
+                        logger.debug(f'Sending a message {client_message} to {to_client.getpeername()}')
+                    else:
+                        logger.debug('This user is not online!')
             except TypeError as e:
                 logger.exception(f'Is message a dict?')
-            except:
+            except Exception as e:
+                logger.debug(e)
                 logger.info(f'Client {sock.fileno()} {sock.getpeername()} was disconnected.')
                 sock.close()
                 all_clients.remove(sock)
@@ -75,6 +94,7 @@ def main():
         logger.info(f'Server is running on {host}:{str(port)}')
         s.listen(5)
         s.settimeout(0.2)
+        names = {}
         while True:
             try:
                 client, addr = s.accept()
@@ -83,18 +103,18 @@ def main():
             else:
                 logger.debug(f'New client: {addr}')
                 clients.append(client)
-            finally:
-                wait = 1
-                r = []
-                w = []
-                try:
-                    r, w, e = select.select(clients, clients, [], wait)
-                except:
-                    pass
 
-                responses = read_requests(r, clients)
-                if responses:
-                    write_responses(responses, w, clients)
+            wait = 1
+            r = []
+            w = []
+            try:
+                if clients:
+                    r, w, e = select.select(clients, clients, [], wait)
+            except OSError:
+                pass
+            responses = read_requests(r, clients, names)
+            if responses:
+                write_responses(responses, w, clients, names)
 
 
 if __name__ == '__main__':
